809cf73cf1bda4ae9032d3da5b5b39088d3baa13
o.u24h <Np7s.shop@outlook.de>
2025-07-22 21:53:08 +0300

    Update interactions.js

--- api/interactions.js
+++ api/interactions.js
@@ -1,8 +1,31 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    // تحقق من التوقيع ثم نفّذ الأمر
-    res.status(200).send('pong!');
-  } else {
-    res.status(405).send('Method Not Allowed');
+import { verifyKey } from 'discord-interactions';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
   }
+
+  const signature = req.headers['x-signature-ed25519'];
+  const timestamp = req.headers['x-signature-timestamp'];
+  const rawBody = await getRawBody(req); // تحتاج مكتبة raw-body
+
+  const isValid = verifyKey(rawBody, signature, timestamp, process.env.PUBLIC_KEY);
+
+  if (!isValid) {
+    return res.status(401).send('Invalid signature');
+  }
+
+  const json = JSON.parse(rawBody);
+
+  if (json.type === 1) {
+    return res.status(200).json({ type: 1 }); // PONG
+  }
+
+  // رد فعل أو أمر Slash
 }
